de5df168f2ac1c7665186e49bc97c6bd0b1879f8
GarzonMiguelA <miguel.garzon.7e7@itb.cat>
2024-11-12 16:03:55 +0100

    commit

--- Burguer_ITB/src/components/Pedidos.vue
+++ Burguer_ITB/src/components/Pedidos.vue
@@ -1,10 +1,10 @@
 <script setup>
 // Importamos funciones que utilizaremos:
 import { inject, defineEmits } from 'vue';
+import Currency from './Currency.vue';
 
 // Inyectamos productos y la moneda:
 const productos = inject('productos');
-const currency = inject('currency');
 
 // Emitimos eventos
 const emit = defineEmits();
@@ -12,21 +12,19 @@ const emit = defineEmits();
 // Creamos la funcion para formatear precio
 function formateoDePrecios(precio) {
    // Formateamos precio usando la moneda seleccionada y aseguramos que tenga dos decimales (para conseguir .00):
-   return `${currency.value}${precio.toFixed(2)}`;
+   return `${precio.toFixed(2)}`;
 }
 
-// Creamos la función para manejar el evento de añadir al carro
+// Creamos función para manejar el evento de añadir al carro
 function addToCart(product) {
    // Emitimos un evento 'product-added' con el nombre del producto seleccionado
    emit('product-added', product.name); // Emitir el nombre del producto
 }
 </script>
 
-
-
 <template>
    <div v-for="product in productos" :key="product.name">
-     <span>{{ product.name }}: {{ formateoDePrecios(product.price) }}</span>
+     <span> {{ product.name }}: <Currency/> {{ formateoDePrecios(product.price) }} </span>
      <button @click="addToCart(product)">Add to Cart</button>
    </div>
 </template>
